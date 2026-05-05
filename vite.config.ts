81123ae2caa07e9f99e6add484e1880979252bfc
guttikondaV <121766269+guttikondaV@users.noreply.github.com>
2026-05-05 16:29:12 -0500

    fix: Move to central repo so that domain works

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/srivenpros/",
+  // base: mode === "development" ? "/" : "/srivenpros/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
