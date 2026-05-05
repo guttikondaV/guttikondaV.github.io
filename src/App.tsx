dc11eef332a94ac88a8c034d412b010a586ad47a
Varun Guttikonda <guttikonda.v@northeastern.edu>
2026-05-05 15:05:21 -0500

    fix: Added new site pathname for github builds

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,14 @@ import NotFound from "./pages/NotFound.tsx";
 
 const queryClient = new QueryClient();
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
